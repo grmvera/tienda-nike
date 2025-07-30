37b15fa3ff7a6a81504837de73e2aa01c81bab7a
grmvera <grm.vera@yavirac.edu.ec>
2025-07-29 21:29:43 -0500

    implementacion de carrusel en el home

--- tienda-nike/src/app/pages/home/home.component.ts
+++ tienda-nike/src/app/pages/home/home.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { OwlOptions } from 'ngx-owl-carousel-o';
 import { CarouselModule } from 'ngx-owl-carousel-o';
+import { Zapato } from '../../models/zapato.model';
+import { ZAPATOS_DATA } from '../../data/zapatos.data';
 
 @Component({
   selector: 'app-home',
@@ -11,5 +12,19 @@ import { CarouselModule } from 'ngx-owl-carousel-o';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  zapatos: Zapato[] = ZAPATOS_DATA;
+
+  customOptions = {
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    dots: true,
+    nav: true,
+    responsive: {
+      0: { items: 1 },
+      600: { items: 2 },
+      1000: { items: 3 }
+    }
+  };
 
 }
