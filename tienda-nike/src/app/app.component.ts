a444f623d94c7ca07a574cda6dd8dbceeac2e5f6
grmvera <grm.vera@yavirac.edu.ec>
2025-07-29 20:12:30 -0500

    correcion de errores en dependincia y ajustes en estilos

--- tienda-nike/src/app/app.component.ts
+++ tienda-nike/src/app/app.component.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-root',
@@ -9,7 +15,12 @@ import { CommonModule } from '@angular/common';
   imports: [
     RouterModule,
     NavbarComponent,
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
--- tienda-nike/src/app/components/navbar/navbar.component.ts
+++ tienda-nike/src/app/components/navbar/navbar.component.ts
@@ -2,14 +2,22 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
     RouterModule,
+    CommonModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
   ],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
